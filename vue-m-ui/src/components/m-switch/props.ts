6bf9fcb20ec7b622bd8ec82854bcc1284c00b5d6
Flamur Fazliu <flamur.biz@hotmail.com>
2023-06-02 15:15:01 +0200

    feat: Added the switch component 🧈🎉

---
+++ vue-m-ui/src/components/m-switch/props.ts
@@ -0,0 +1,44 @@
+export const colors = {
+    'default': 'm-switch-primary',
+    'light': 'm-switch-light',
+    'dark': 'm-switch-dark',
+
+    'primary': 'm-switch-primary',
+    'secondary': 'm-switch-secondary',
+    'success': 'm-switch-success',
+    'warning': 'm-switch-warning',
+    'danger': 'm-switch-danger',
+    'info': 'm-switch-info',
+   
+    'indigo': 'm-switch-indigo',
+    'purple': 'm-switch-purple',
+    'pink': 'm-switch-pink',
+};
+
+export const knobColors = {
+    'default': 'm-switch-default',
+    'light': 'm-switch-light',
+    'dark': 'm-switch-dark',
+
+    'primary': 'm-switch-primary',
+    'secondary': 'm-switch-secondary',
+    'success': 'm-switch-success',
+    'warning': 'm-switch-warning',
+    'danger': 'm-switch-danger',
+    'info': 'm-switch-info',
+   
+    'indigo': 'm-switch-indigo',
+    'purple': 'm-switch-purple',
+    'pink': 'm-switch-pink',
+};
+
+export const sizes = {
+    'default': 'm-switch-size-md',
+    '2xs': 'm-switch-size-2xs',
+    'xs':  'm-switch-size-xs',
+    'sm': 'm-switch-size-sm',
+    'md': 'm-switch-size-md',
+    'lg': 'm-switch-size-lg',
+    'xl': 'm-switch-size-xl',
+    '2xl': 'm-switch-size-2xl',
+};
