61d412bfc96c36c0f0d4fd9db20f61456eaa715a
Flamur Fazliu <flamur.biz@hotmail.com>
2023-06-02 15:12:59 +0200

    Added the cssnano for minification on web and ui

--- vue-m-ui/postcss.config.js
+++ vue-m-ui/postcss.config.js
@@ -1,6 +1,11 @@
 export default {
   plugins: {
+    cssnano: [ 'default', {
+      discardComments: {
+        removeAll: true,
+      },
+    } ],
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
